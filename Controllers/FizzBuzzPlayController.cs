3228183da6f73d22502b7028f75a9b1dc8fb2c90
Skorska <aggie.skorska2@kingfisher.com>
2024-11-27 09:04:15 +0000

    Done basic task

--- Controllers/FizzBuzzPlayController.cs
+++ Controllers/FizzBuzzPlayController.cs
@@ -31,25 +31,75 @@ namespace FizzBuzzApi.Controllers
             for (i = 0; i < count; i++)
             {
                 fbResultArr[i] = new FizzBuzzResult();
-                fbResultArr[i].value = FizzBuzzCheckRules(start + i);
+                fbResultArr[i].value = FizzBuzzCheckRules(start + i, start);
             }
             return fbResultArr;
         }
+        private long AssessIfPower(long value, long rule) {
+            if (value < rule) {
+                return value;
+            }
+            while (value % rule == 0 )
+            {
+                value /= rule;
+            }
+            return (value == 1) ? 0 : value;
 
-        private string FizzBuzzCheckRules(long value)
+        }
+        // private long AssessIfFibonacci(long value, long start) {
+        // // TODO: Fix!
+        //     long a = start;
+        //     long b = start * 2;
+        //     long c = 0;
+        //     if (value % start <= 1){
+        //         return 0;
+        //     }
+        //     while (a < value)
+        //     {
+        //         c = a + b;
+        //         a = b;
+        //         b = c;
+        //     }
+        //     return (c == value) ? 0 : value;
+
+        // }
+
+        private string ReplaceAllRules(long value, IEnumerable<FizzBuzzRule> ruleList, string suffix) {
+            var res = "";
+             for (int i = 0; i < ruleList.Count(); i++)
+                {
+                    FizzBuzzRule rule = ruleList.ToList().ElementAt(i);
+                    res += rule.ReplaceWith + suffix;
+                }
+            return res;
+        }
+        private string FizzBuzzCheckRules(long value, long start)
         {
             string result = "";
             var allRules = _context.FizzBuzzRules.ToList();
+            var powers = allRules.Where(r => (AssessIfPower(value, r.Id) == 0));
             var rules = allRules.Where(r => ((value % r.Id) == 0 ));
-
+            var lastDigRules = allRules.Where(r => ((value % 10) == r.Id ));
+            // var fibonacci = AssessIfFibonacci(value, start);
+            // Console.Write("Fibonacci: " + value + (fibonacci == 0));
             var count = rules.Count();
+            var powerCount = powers.Count();
+            var digCount = lastDigRules.Count();
+            
+            if (powerCount > 0) {
+                result += ReplaceAllRules(value, powers, "Power");
+            }
+            if (digCount > 0)
+            {
+                result += ReplaceAllRules(value, rules, "DIGthis!");
+            }
+            // if (fibonacci == 0)
+            // {
+            //     result += "Fibonacci";
+            // }
             if (count > 0)
             {
-                for (int i = 0; i < count; i++)
-                {
-                    FizzBuzzRule rule = rules.ToList().ElementAt(i);
-                    result += rule.ReplaceWith;
-                }
+                result += ReplaceAllRules(value, rules, "");
             }
             else
             {
